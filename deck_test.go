4b5a92808635731b787ebe31aba780add250308c
Ammce <amcenp@gmail.com>
2019-12-18 16:39:32 +0100

    Some more tests

--- deck_test.go
+++ deck_test.go
@@ -7,4 +7,10 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 20 {
 		t.Errorf("Error happened, expecting 20 cards, but got %v", len(d))
 	}
+	if d[0] != "Ace of Diamond" {
+		t.Errorf("Error Happened, expecting Ace of Diamond, but got %v", d[0])
+	}
+	if d[len(d)-1] != "Four of Club" {
+		t.Errorf("Error happened, expecting Four of Club, bug tog %v", d[len(d)-1])
+	}
 }
