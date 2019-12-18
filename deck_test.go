918046c0c406db11e4a5feda4abe1e8ab7b94aba
Ammce <amcenp@gmail.com>
2019-12-18 17:04:17 +0100

    Testing Done and Project Done

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -13,4 +16,18 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Club" {
 		t.Errorf("Error happened, expecting Four of Club, bug tog %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndGetCardsFromFile(t *testing.T) {
+	fileName := "_deckTesting"
+	os.Remove(fileName)
+	d := newDeck()
+	d.saveToFile(fileName)
+
+	loadedDeck := getCardsFromFile(fileName)
+
+	if len(loadedDeck) != 20 {
+		t.Errorf("Error Happened, Expected 20 cards, but got %v", len(loadedDeck))
+	}
+	os.Remove(fileName)
 }
